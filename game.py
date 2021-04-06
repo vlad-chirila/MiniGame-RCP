f59a542bec9fb0725d210a5b9416e9d25f883f4f
VladChirila <vlad.chirila@live.com>
2021-04-06 17:59:14 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,13 +1,13 @@
 import random;
 
 def Random_choice():
-  return random.choice(['rock','scissors','paper']);
+  return random.choice(['rock','scissor','paper']);
 
 def Convert_input(your_choice):
   if your_choice == 1:
     your_choice = 'rock';
   elif your_choice == 2:
-    your_choice = 'scissors';
+    your_choice = 'scissor';
   elif your_choice == 3:
     your_choice = 'paper';
   else:
@@ -18,17 +18,17 @@ def Convert_input(your_choice):
 def Play(computer_choice, your_choice):
   if computer_choice == your_choice:
     print("TIE");
-  elif computer_choice == 'scissors' and your_choice == 'rock':
+  elif computer_choice == 'scissor' and your_choice == 'rock':
     print("WIN");
   elif computer_choice == 'rock' and your_choice == 'paper':
     print("WIN");
-  elif computer_choice == 'paper' and your_choice == 'scissors':
+  elif computer_choice == 'paper' and your_choice == 'scissor':
     print("WIN");
   else:
     print("Computer wins!");
 
 def Main():
-  print(" 1 - rock \n 2 - scissors \n 3 - paper \n");
+  print(" 1 - rock \n 2 - scissor \n 3 - paper \n");
   computer_choice = Random_choice();
   your_choice = Convert_input(int(input("Choose: ")));
   Play(computer_choice, your_choice);
